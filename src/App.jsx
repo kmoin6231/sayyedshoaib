bac197c85afe0b522dd7f38ecbcfae3a43059de3
kmoin6231@gmail.com <kmoin6231@gmail.com>
2025-06-25 12:09:07 +0530

    Rename Footer to SiteFooter to fix export issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import Footer from './components/Footer'
+import Footer from './components/SiteFooter'
 import Home from './pages/Home'
 
 function App() {
