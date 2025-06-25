bbc2d94e7c19b61c755d6744b5d99edfb0511f7f
kmoin6231@gmail.com <kmoin6231@gmail.com>
2025-06-25 13:22:28 +0530

    Fix footer import path to use SiteFooter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import Footer from './components/SiteFooter';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
