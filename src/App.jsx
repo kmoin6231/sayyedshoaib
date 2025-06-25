5dcb6da3c37f2d2a2fec05acf8f64903fe53a60e
kmoin6231@gmail.com <kmoin6231@gmail.com>
2025-06-25 11:44:07 +0530

    Fix App.jsx export issue

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">
+        <Home />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
